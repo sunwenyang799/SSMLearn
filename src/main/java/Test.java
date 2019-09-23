995d6ffc7da0c1ee2109ef66b1336c35e96cd33a
sunwenyang <sunwenyang@thunisoft.com>
2019-09-23 16:24:39 +0800

    111

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,7 @@
 /*
- * @(#)Test.java 2019年9月23日下午4:12:49
- * SSMLearn
- * Copyright 2019 Thuisoft, Inc. All rights reserved.
- * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * @(#)Test.java 2019年9月23日下午4:12:49 SSMLearn Copyright 2019 Thuisoft, Inc. All
+ * rights reserved. THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to
+ * license terms.
  */
 
 /**
@@ -12,5 +11,7 @@
  *
  */
 public class Test {
-
+    public static void main(String[] args) {
+        System.out.print("成功");
+    }
 }
