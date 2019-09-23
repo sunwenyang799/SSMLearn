88e97cab667c75cdb95e7246c197b60343a0f87e
sunwenyang <sunwenyang@thunisoft.com>
2019-09-23 16:14:27 +0800

    Test

---
+++ src/main/resources/Test.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)Test.java 2019年9月23日下午4:12:57
+ * SSMLearn
+ * Copyright 2019 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * Test
+ * @author lenovo
+ * @version 1.0
+ *
+ */
+public class Test {
+
+}
